ba677393e175105d4c06b71d2b8483fca7d115f2
Arunachalam <arun@gogosoon.com>
2023-05-13 16:23:08 +0530

    [Arunachalam] LIst resources on opening the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -28,12 +30,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> _resources = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchResources();
+  }
+
+  void _fetchResources() async {
+    final response = await http.get(Uri.parse(
+        'https://api.sampleapis.com/codingresources/codingResources'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List<dynamic>;
+      setState(() {
+        _resources = data;
+      });
+    } else {
+      throw Exception('Failed to load resources');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _resources.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: _resources.length,
+                itemBuilder: (context, index) {
+                  final resource = _resources[index];
+                  return InkWell(
+                      onTap: () => {},
+                      child: Card(
+                          child: ListTile(
+                        title: Text(resource['description']),
+                        subtitle: Text(resource['url']),
+                        leading: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                        trailing: Text(resource['types'].join(', ')),
+                      )));
+                }));
   }
 }
